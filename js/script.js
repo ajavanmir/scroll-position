865eb69b6f8a084f1645272c87473352a3ece8a4
amir javanmir <74532873+ajavanmir@users.noreply.github.com>
2023-10-12 04:25:16 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const progress = document.querySelector(".progress-bar");
+window.addEventListener("scroll", ()=>{
+    const scrollPoint = window.scrollY;
+    const htmlHeight = document.documentElement.getClientRects()[0].height;
+    const winInner = window.innerHeight;
+    const res = Math.floor(100/((htmlHeight-winInner)/scrollPoint));
+    progress.style.width = `${res}%`
+})
